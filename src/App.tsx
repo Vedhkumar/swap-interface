eff371ce1cfb635c129457c8387da447c3aa22f3
Vedh Kumar <vedhkumar237@gmail.com>
2025-02-25 17:32:01 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+ import { WagmiProvider } from 'wagmi'
+ import { config } from './config'
+ 
+ const queryClient = new QueryClient()
+ 
+ function App() {
+   return (
+     <WagmiProvider config={config}>
+       <QueryClientProvider client={queryClient}> 
+        Hello
+       </QueryClientProvider> 
+     </WagmiProvider>
+   )
+ }
+
+ export default App
